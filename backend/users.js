0543436cce6b438d03e5f7a14b25d6568e0ea7c4
Carlos Revilla <clro26.k@gmail.com>
2020-08-14 23:30:23 +0200

    user search by id to remove it from the rooms

--- backend/users.js
+++ backend/users.js
@@ -31,12 +31,9 @@ const removeUser = (id) => {
     if (index !== -1) {
         return users.splice(index, 1)[0];
     }
-
 }
 
-const getUser = () => {
-
-}
+const getUser = (id) => users.find((user) => user.id === id);
 
 const getUsersInRoom = () => {
 
