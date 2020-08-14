5a3d9646e94dedd362325f611c1777ac97970559
Carlos Revilla <clro26.k@gmail.com>
2020-08-15 00:51:50 +0200

    emit msg and send it to the room

--- backend/index.js
+++ backend/index.js
@@ -43,7 +43,18 @@ io.on('connection', (socket) => {
             text: `${user.name}, has joined!`
         });
         socket.join(user.room);
+
+        callback();
     });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+        io.to(user.room).emit('message', {
+            user: user.name,
+            text: message
+        });
+        callback();
+    })
 
     socket.on('disconnect', () => {
         console.log('User had left!!');
