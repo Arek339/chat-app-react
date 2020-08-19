8dbe17663c51f2cce104d593d5251b8e1d7fe8f0
Carlos Revilla <clro26.k@gmail.com>
2020-08-19 15:35:07 +0200

    refactoring

--- frontend/src/components/Messages/Message/Message.js
+++ frontend/src/components/Messages/Message/Message.js
@@ -4,7 +4,7 @@ import './Message.css';
 
 import ReactEmoji from 'react-emoji';
 
-const Message = ({ message:{ user , text }, name}) => {
+const Message = ({ message: { text, user }, name}) => {
     let isSentByCurrentUser = false;
 
     const trimmedName = name.trim().toLowerCase();
@@ -16,7 +16,7 @@ const Message = ({ message:{ user , text }, name}) => {
     return(
         isSentByCurrentUser
         ? (
-            <div className="messageContainer justtifyEnd">
+            <div className="messageContainer justifyEnd">
                 <p className="sentText pr-10"> {trimmedName}</p>
                 <div className="messageBox backgroundBlue">
                 <p className="messageText colorWhite">{ReactEmoji.emojify (text)}</p>
@@ -24,14 +24,14 @@ const Message = ({ message:{ user , text }, name}) => {
             </div>
         )
         : (
-            <div className="messageContainer justtifyStart">
+            <div className="messageContainer justifyStart">
                 <div className="messageBox backgroundLight">
                 <p className="messageText colorDark">{ReactEmoji.emojify (text)}</p>
                 </div>
                 <p className="sentText pl-10"> {user}</p>
             </div>
         )
-    )
+    );
 }
 
 export default Message;
