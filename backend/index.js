8f4ed69222ab02f8d1a6b1779f72b6c6aa8970be
Carlos Revilla <clro26.k@gmail.com>
2020-08-19 12:24:00 +0200

    route redirection, socket method and integration of arrow functions

--- backend/index.js
+++ backend/index.js
@@ -2,38 +2,27 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
-const {
-    addUser,
-    removeUser,
-    getUser,
-    getUsersInRoom
-} = require('./users.js');
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
 
 const PORT = process.env.PORT || 5000
 
 const router = require('./router');
-const {
-    use
-} = require('./router');
+const {use} = require('./router');
+const {emit} = require('process');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-io.on('connection', (socket) => {
-    socket.on('join', ({
-        name,
-        room
-    }, callback) => {
-        const {
-            error,
-            user
-        } = addUser({
-            id: socket.id,
-            name,
-            room
-        });
+app.use(router);
+
+io.on('connect', (socket) => {
+    socket.on('join', ({ name, room }, callback) => {
+        const { error, user } = addUser({ id: socket.id, name, room });
+
         if (error) return callback(error);
+        socket.join(user.room);
+
         socket.emit('message', {
             user: 'admin',
             text: `${user.name}, welcome to the room ${user.room}`
@@ -42,7 +31,11 @@ io.on('connection', (socket) => {
             user: 'admin',
             text: `${user.name}, has joined!`
         });
-        socket.join(user.room);
+
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        });
 
         callback();
     });
@@ -54,13 +47,23 @@ io.on('connection', (socket) => {
             text: message
         });
         callback();
-    })
+    });
 
     socket.on('disconnect', () => {
-        console.log('User had left!!');
+        const user = removeUser(socket.id);
+
+        if (user) {
+            io.to(user.room).emit('message', {
+                user: 'admin',
+                text: `${user.name} has left.`
+            });
+            io.to(user.room).emit('roomData', {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            });
+        }
     })
 });
 
-app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
